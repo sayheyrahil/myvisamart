509408f6fe555350df615aa9f5c54bf42bae5d7d
juhi-developer <developer.juhi@gmail.com>
2025-11-26 22:43:06 +0530

    solve heres eom issue

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import  Header from '@/components/layouts/header';
+import Header from '@/components/layouts/header';
 import Footer from '@/components/layouts/footer';
 import Categories from '@/components/common/home/categories';
 import HeroSection from '@/components/common/home/HeroSection';
@@ -9,25 +9,27 @@ import PopularDestinations from '@/components/common/home/PopularDestinations';
 import TopDestinations from '@/components/common/home/TopDestinations';
 import WhyVisamart from '@/components/common/home/WhyVisamart';
 import NewsletterSection from '@/components/common/home/NewsletterSection';
+import MasterPage from '@/components/layouts/master';
 
- 
+
 
 export default function Home() {
- 
+
 
   return (
-    
-     <div className="min-h-screen bg-[#F6FBFF] ">
-      <Header />
-      <HeroSection  />
-      <Categories />
-      <HowItWorks />
-      <PopularDestinations />
-      <TopDestinations />
-      <WhyVisamart />
-      <NewsletterSection />
-      <Footer />
-     
+
+    <div className="min-h-screen bg-[#F6FBFF] ">
+      <MasterPage title="MyVisaMart - Your Gateway to Hassle-Free Visa Applications">
+
+        <HeroSection />
+        <Categories />
+        <HowItWorks />
+        <PopularDestinations />
+        <TopDestinations />
+        <WhyVisamart />
+        <NewsletterSection />
+      </MasterPage>
+
     </div>
   );
 }
